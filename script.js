b2867dca1c076c05d3b1934148dd8f7cc26f1475
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-02-06 14:33:24 +0100

    🪓 migrate to ethers v6
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Author: Pascal Marco Caversaccio
-// E-Mail: pascal.caversaccio@hotmail.ch
+const { seedPhrase } = require('./secrets.json');
+const { HDNodeWallet } = require('ethers');
 
-const { seedPhrase } = require('./secrets.json'); // Include the wallet mnemonic
-const ethers = require('ethers') // Import the ethers.js library, a complete Ethereum wallet implementation and utilities for JavaScript (and TypeScript)
-
-let mnemonic = seedPhrase; // Set the wallet mnemonic
-let mnemonicWallet = ethers.Wallet.fromMnemonic(mnemonic); // Generates a BIP-039 + BIP-044 wallet from mnemonic deriving path (default = "m/44'/60'/0'/0/0") using the wordlist
-console.log(mnemonicWallet.privateKey); // Output the private key
+// Generates a BIP-039 + BIP-044 wallet from mnemonic deriving path
+// (default = "m/44'/60'/0'/0/0") using the wordlist.
+const mnemonicWallet = HDNodeWallet.fromPhrase(seedPhrase);
+console.log(mnemonicWallet.privateKey);
